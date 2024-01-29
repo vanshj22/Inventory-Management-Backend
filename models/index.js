2df2fdd5643fbb9d6f9e89319a11d727757bc39b
vanshj22 <vanshjain5232@gmail.com>
2024-01-29 19:48:40 +0400

    Update MongoDB connection URI

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://admin:RoXNgneedWZDrUOn@cluster0.ypwz577.mongodb.net/?retryWrites=true&w=majority";
-
-
+// const uri = "mongodb+srv://admin:RoXNgneedWZDrUOn@cluster0.ypwz577.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://qwerty321:mydatabase123@myinventory.ebjtqkj.mongodb.net/?retryWrites=true&w=majority"
 function main() {
     mongoose.connect(uri).then(() => {
         console.log("Succesfull")
